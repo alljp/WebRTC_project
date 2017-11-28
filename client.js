045c68169225bce76010d7eba3e2a29cef3c6514
alljp <allenjosesmailbox@gmail.com>
2017-11-28 21:29:29 +0530

    Add check for browser support of WebRTC

--- client.js
+++ client.js
@@ -1,5 +1,12 @@
+
 function returnUserMedia () {
    // check if the browser supports the WebRTC
   return (navigator.getUserMedia || navigator.webkitGetUserMedia ||
       navigator.mozGetUserMedia)
+}
+
+if (navigator.getUserMedia = returnUserMedia()) {
+  alert('WebRTC is supported. Select Allow to get video')
+} else {
+  alert('WebRTC is not supported')
 }
